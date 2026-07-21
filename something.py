f5ab711eaabce50afa3d174fa78b971ea5a31a26
dimar10 <ukozlova922@gmail.com>
2026-07-21 10:09:19 +0300

    leetcode

--- something.py
+++ something.py
@@ -75,3 +75,71 @@ users = [
 
 n  = list(filter(lambda x : x['age']>=18,users))
 print(n)#dict
+'===================================================================='
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        value ={}
+        for i,num in enumerate(nums):
+            complement = target - num
+            if complement in value:
+                return [value[complement],i]
+            value[num] = i
+        return []
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x<0:
+            return False
+        s = str(x)
+        return s == s[::-1]
+
+
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        roman = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+        total = 0
+
+        for i in range(len(s)):
+            if i < len(s) - 1 and roman[s[i]] < roman[s[i + 1]]:
+                total -= roman[s[i]]
+            else:
+                total += roman[s[i]]
+
+        return total
+
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        if not strs:
+            return ''
+
+        prefix = strs[0]
+        for s in strs[1:]:
+            while not s.startswith(prefix):
+                prefix = prefix[:-1]
+            if not prefix:
+                return ''
+        return prefix
+
+
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode(-1)
+        current = dummy
+
+        while list1 and list2:
+            if list1.val <= list2.val:
+                current.next = list1
+                list1 = list1.next
+            else:
+                current.next = list2
+                list2 = list2.next
+            current = current.next
