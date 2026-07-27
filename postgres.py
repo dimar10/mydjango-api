84e258e583a7d2ac7fe858af5d26c54e09f98b13
dimar10 <ukozlova922@gmail.com>
2026-07-27 18:03:26 +0300

    complete

--- postgres.py
+++ postgres.py
@@ -75,6 +75,78 @@ from Weather w1
 join Weather w2 on w1.recordDate = w2.recordDate + INTERVAL '1 day'
 where w1.temperature > w2.temperature; вывести температуру самую высокую с разницей в 1 день с прошлой
 
+=========================================================================================================
+
+
+SELECT player_id, MIN(event_date) AS first_login
+FROM Activity
+GROUP BY player_id; # дернуть первое вхождение плэера
+
+select name
+from Customer
+where referee_id !=2 or referee_id is null; чето типо рефералки
+
+SELECT customer_number
+FROM Orders
+GROUP BY customer_number
+order by count(*) desc
+limit 1; макс колво заказов от кастомера и вывести его айди
+
+
+select name, population,area
+from World
+where area >= 3000000 or  population > 25000000; условия
+
+
+select class
+from Courses
+group by class
+having count(*)>5; дернуть того чье колво больше пяти в классе
+
+
+SELECT name
+FROM SalesPerson
+WHERE sales_id NOT IN (
+    SELECT o.sales_id
+    FROM Orders o
+    JOIN Company c ON o.com_id = c.com_id
+    WHERE c.name = 'RED'
+);сложно надо думать
+
+select x,y,z,
+    case
+        when x + y > z and x + z > y and z + y > x then 'Yes'
+        else 'No'
+    end as triangle
+from Triangle # чекнули треугольники
+======================================================================
+
+SELECT
+    score,
+    DENSE_RANK() OVER (ORDER BY score DESC) AS "rank"
+FROM Scores
+ORDER BY score DESC;
+
+
+
+select distinct l1.num as ConsecutiveNums
+from Logs l1
+join Logs l2 on l1.id = l2.id-1 and l1.num =l2.num
+join Logs l3 on l1.id = l3.id -2 and l1.num =l3.num
+
+
+
+SELECT d.name AS Department, e.name AS Employee, e.salary AS Salary
+FROM Employee e
+JOIN Department d ON e.departmentId = d.id
+WHERE (e.departmentId, e.salary) IN (
+    SELECT departmentId, MAX(salary)
+    FROM Employee
+    GROUP BY departmentId
+);подзапрос
+
+
+
 
 
 
